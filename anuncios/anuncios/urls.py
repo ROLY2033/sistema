83c92c2a3fa6aa5331034ae449fb7cf235e3c897
rolya2018 <studentofsystems@gmail.com>
2021-12-20 17:35:34 -0500

    video 3

--- anuncios/anuncios/urls.py
+++ anuncios/anuncios/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from anuncios.views import saludo , data
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludo),
+    path('final/', data),
 ]
